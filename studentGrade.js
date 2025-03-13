463264f52c22109df5c16902671d930353154476
Esther-Sonia <esthersonia21@gmail.com>
2025-03-13 16:13:24 +0300

    adds StudentGrade function

---
+++ studentGrade.js
@@ -0,0 +1,8 @@
+function studentGradeGenerator(marks) {
+    if (marks > 79) return 'A';
+    if (marks >= 60) return 'B';
+    if (marks >= 50) return 'C';
+    if (marks >= 40) return 'D';
+    return 'E';
+}
+console.log(studentGradeGenerator(45));
